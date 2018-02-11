f628bd85de9742a55c8aa7e6b96b4cd2c6d707f9
s11rikuya <rikuya.suzuki@gaiax.com>
2018-02-11 16:09:20 +0900

    git移転

---
+++ app.rb
@@ -0,0 +1,27 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: './bbs.db'
+)
+
+class Comment < ActiveRecord::Base
+  validates :body, presence: true
+end
+
+get '/' do
+  @title = "My BBS"
+  @comments = Comment.all
+  erb :index
+end
+
+post '/create' do
+  Comment.create(body: params[:body])
+  redirect to('/')
+end
+
+post '/destroy' do
+  Comment.find(params[:id]).destroy
+end
